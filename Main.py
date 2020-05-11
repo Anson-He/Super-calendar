ec8092fc1ec4e249a1a53b254f1ad137cd7432a6
Anson-He <1597893898@qq.com>
2020-05-12 00:50:05 +0800

    主体框架构建好了 加入了播放音乐的功能 在Main.py中右键运行，点击播放音乐按钮，选定即可

--- Main.py
+++ Main.py
@@ -1,15 +1,20 @@
-import tkinter as tk  # 使用Tkinter前需要先导入
-# 第1步，实例化object，建立窗口window
+import tkinter as tk
+from music import *
 window = tk.Tk()
-# 第2步，给窗口的可视化起名字
-window.title('My Window')
-# 第3步，设定窗口的大小(长 * 宽)
-window.geometry('1000x500')  # 这里的乘是小x
-# 第4步，在图形界面上设定标签
-l = tk.Label(window, text='超级万年历', font=('Arial', 12), width=30, height=2)
-# 说明： bg为背景，font为字体，width为长，height为高，这里的长和高是字符的长和高，比如height=2,就是标签有2个字符这么高
-# 第5步，放置标签
-l.pack()    # Label内容content区域放置位置，自动调节尺寸
-# 放置lable的方法有：1）l.pack(); 2)l.place();
-# 第6步，主窗口循环显示
+window.title('超级万年历')#给窗口的可视化起名字
+window.geometry('1000x500')# 设定窗口的大小(长 * 宽)
+calendar = tk.Text(window)#日历窗口
+calendar.place(x=10, y=50, width=500, height=300)
+current_time = tk.Text(window)#时间窗口
+current_time.place(x=625,y=20,width=300, height=50)
+button_weather = tk.Button(window, text='天气', font=('Arial', 12), width=15, height=3) #command=weather
+button_weather.place(x=600,y=100)
+button_memo = tk.Button(window, text='备忘录', font=('Arial', 12), width=15, height=3) #command=memo
+button_memo.place(x=800,y=100)
+button_lunar = tk.Button(window, text='农历', font=('Arial', 12), width=15, height=3) #command=lunar
+button_lunar.place(x=600,y=200)
+button_else = tk.Button(window, text='其他', font=('Arial', 12), width=15, height=3) #command=else
+button_else.place(x=800,y=200)
+button_music = tk.Button(window, text='▶', font=('Arial', 25), width=5, height=1,command = music)
+button_music.place(x=700,y=300)
 window.mainloop()
---
+++ music.py
@@ -0,0 +1,44 @@
+from music_button import *
+import tkinter as tk
+from tkinter import scrolledtext
+from tkinter import ttk
+from mutagen.mp3 import MP3
+from PIL import Image,ImageTk
+import pygame
+import os.path
+import glob
+def music():
+
+    pygame.init()
+    pygame.mixer.init()
+    def play_current_song(*args):
+        """ 播放当前列表歌曲 """
+        indexs = play_list.curselection()
+        selectindex = int(indexs[0])
+        pygame.mixer.music.load(file_list[selectindex])
+        pygame.mixer.music.play()
+    def play():
+        pygame.mixer.music.unpause()
+    def pause():
+        pygame.mixer.music.pause()
+
+    window = tk.Tk()
+    window.title("播放音乐")
+    window.geometry("300x500")
+    #导入本地歌曲
+    realpath = os.path.realpath(__file__) #当前绝对路径
+    dirname = os.path.dirname(realpath)
+    extension = 'mp3'
+    file_list = glob.glob('*.'+extension) #返回所有MP3文件
+    #播放列表可视化
+    play_list = tk.Listbox(window,width=30,height=20)
+    for i in file_list:
+        play_list.insert('end',i)
+
+    play_list.bind("<<ListboxSelect>>",play_current_song)#点击列表,播放对应歌曲
+    play_list.pack()
+
+    play_button = tk.Button(window, text='▶', font=('Arial', 10), width=2, height=1,command = play)#播放按钮
+    play_button.place(x=150,y=380)
+    pause_button = tk.Button(window, text='⏸', font=('Arial', 10), width=2, height=1,command = pause)#暂停按钮
+    pause_button.place(x=100,y=380)
