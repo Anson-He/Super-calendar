1c04cd2f6a1087fcd563544d5411e8b9a25fcf7b
Anson-He <1597893898@qq.com>
2020-05-12 13:25:46 +0800

    增加了时间模块，日历初步测试

--- Main.py
+++ Main.py
@@ -1,12 +1,74 @@
 import tkinter as tk
+import tkinter.font as tf
+
+import time
+
+localtime = time.localtime(time.time())#(tm_year, tm_mon, tm_mday, tm_hour, tm_min, tm_sec, tm_wday, tm_yday, tm_isdst)
+
 from music import *
+
 window = tk.Tk()
-window.title('超级万年历')#给窗口的可视化起名字
+window.title('超级万年历')
 window.geometry('1000x500')# 设定窗口的大小(长 * 宽)
+
+#-----------------日历年月日选择-----------
+year = ttk.Combobox(window,width=10)
+year.place(x=65,y=20)
+year_label = tk.Label(window,text='年')
+year_label.place(x=165,y=20)
+month = ttk.Combobox(window,width=10)
+month.place(x=245,y=20)
+month_label = tk.Label(window,text='月')
+month_label.place(x=345,y=20)
+year_list = []
+for i in range(1991,localtime.tm_year+100):
+    year_list.append(i)
+year['value'] = year_list
+year.current(year_list.index(localtime.tm_year))
+month_list = []
+for i in range(1,13):
+    month_list.append(i)
+month['value'] = month_list
+month.current(month_list.index(localtime.tm_mon))
+
+#-----------------------------------------
+
+
+#---------------日历制作----------------------
 calendar = tk.Text(window)#日历窗口
+ft = tf.Font(family='微软雅黑',size=15)
+calendar.tag_add('tag_ym',0.0)
+calendar.tag_config('tag_ym',font =ft )
+calendar.insert(1.1,"%s年 %s月"%(year.get(),month.get())+'\n','tag_ym')
+def get_year_month():
+    calendar.delete(1.0,tk.END)
+    calendar.insert(1.1,"%s年 %s月"%(year.get(),month.get())+'\n','tag_ym')
+    calendar.insert(2.1,"   周日     周一     周二     周三     周四     周五     周六"+'\n','tag_ym')
+button_sure = tk.Button(window, text='确定', font=('Arial', 10), width=7, height=1,command = get_year_month)
+button_sure.place(x=380,y=15)
+calendar.insert(2.1,"   周日     周一     周二     周三     周四     周五     周六"+'\n','tag_ym')
 calendar.place(x=10, y=50, width=500, height=300)
-current_time = tk.Text(window)#时间窗口
-current_time.place(x=625,y=20,width=300, height=50)
+
+
+
+#-----------------动态显示时间-----------------
+def get_time():
+    current_time = time.strftime("%H:%M:%S")
+    time1 = ''
+    time2 = current_time
+    if time2 != time1:
+        time1 = time2
+        current_time_model = tk.Label(window,text = time1,font = 100)
+        current_time_model.config(text = time2)
+        current_time_model.place(x=730,y=25)
+        current_time_model.after(1000,get_time)#每一千毫秒（即一秒）后执行get_time()
+get_time()
+#------------------------------------------------
+
+
+
+
+
 button_weather = tk.Button(window, text='天气', font=('Arial', 12), width=15, height=3) #command=weather
 button_weather.place(x=600,y=100)
 button_memo = tk.Button(window, text='备忘录', font=('Arial', 12), width=15, height=3) #command=memo
